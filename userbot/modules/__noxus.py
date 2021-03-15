4f076063b0d73848f48009c8fd2942b30fa0368b
noxuserbot <80694411+noxuserbot@users.noreply.github.com>
2021-03-15 18:06:50 +0300

    Add files via upload

---
+++ userbot/modules/__noxus.py
@@ -0,0 +1,35 @@
+from userbot.cmdhelp import CmdHelp
+from userbot import cmdhelp
+from userbot import CMD_HELP
+from userbot.events import register
+
+# ██████ LANGUAGE CONSTANTS ██████ #
+
+from userbot.language import get_value
+LANG = get_value("__mbe")
+
+# ████████████████████████████████ #
+
+@register(outgoing=True, pattern="^.mbe(?: |$)(.*)")
+async def mbe(event):
+    """ .mbe komutu için """
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit(LANG["NEED_PLUGIN"])
+    else:
+        string = ""
+        sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
+        
+        for i in sayfa:
+            string += f'`▶️ `'
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await event.edit(LANG["NEED_MODULE"] + '\n\n' + string)
